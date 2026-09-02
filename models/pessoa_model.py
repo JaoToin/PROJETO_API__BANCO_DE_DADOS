a3b25ed6806116e4425b5a64c9a4bb7c489c065d
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-09-02 19:02:32 -0300

    modificado o modelo para encaixar com o meu projeto esport ar livre

--- models/pessoa_model.py
+++ models/pessoa_model.py
@@ -5,12 +5,19 @@ from database import Base
 class Pessoa(Base):
     __tablename__ = "pessoa"
     
-    idpessoa = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(60))
-    datanascimento = Column(Date)
+    data_nascimento = Column(Date)
     peso = Column(Integer)
     altura = Column(DECIMAL(10,2))
     sexo = Column(String(1))
+    cpf = Column(String)
+    cep = Column(Integer)
+    rua_logradouro = Column(String(100))
+    bairro = Column(String(20))
+    cidade = Column(String(70))
+    uf = Column(String(2))
+    
     
     
    
